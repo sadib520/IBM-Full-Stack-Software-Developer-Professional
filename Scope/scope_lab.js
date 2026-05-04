3768bcac26e8c968be39028b90e9df7aad5bed32
sadib520 <sadibhasan520@gmail.com>
2026-05-04 03:22:16 -0400

    JS Lab Practice

---
+++ Scope/scope_lab.js
@@ -0,0 +1,10 @@
+//global scope
+var globalvar = "this is a global variable";
+let a = 122; // block scope
+const b = "sadib";
+const c = 10;
+
+
+console.log(globalvar);
+console.log(a);
+console.log(b);
